7eebe7b23592487bcd6831952f3136c0e2c27a52
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2022-01-11 11:38:58 -0600

    Add files via upload
    fixed the jump animation

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var character = document.getElementById("character"); //sets character to the el
 document.addEventListener("click", jump); // an event listener that starts the function jump
 
 function jump(){
+    if(character.classList == "animate"){return;} // prevents glitch that happens when you click while the character is currently jumping.
     character.classList.add("animate");
-    setTimeout(removejump,300); //300ms = length of animation
+    setTimeout(removeJump,300); //300ms = length of animation
 }; //function jump adds animate class to the character div
-
-if(character.classList == "animate"){return;} // prevents glitch that happens when you click while the character is currently jumping.
 
 function removeJump(){
     character.classList.remove("animate");
