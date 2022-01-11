c543b520ad72e4ada592402f32285afe0a7dca76
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2022-01-11 11:50:15 -0600

    Add files via upload
    finished the game. would like to add a start button so it doesn't just start and finish so quick

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function removeJump(){
     character.classList.remove("animate");
 } //timeout for the jump function
 
+var block = document.getElementById("block"); //created the variable block in js
+
+function checkDead(){ //function to see if block has hit character
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        alert("Game Over");
+    }
+
+}
+
+setInterval(checkDead, 10);
